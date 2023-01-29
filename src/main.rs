fa81071785bfa7bda949d17269121a7d6be746d0
zachjcomer <zachjcomer@gmail.com>
2023-01-28 20:31:10 -0500

    blog API

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use std::{fs::{OpenOptions, File}, io::{Write, BufReader, BufRead}};
 use rocket::{serde::{json::Json}, http::Status, response::{Responder, self}, Request};
 use rocket_db_pools::{Database, Connection};
 
-use rust_rest::note::Note;
-use rust_rest::note::NewNote;
-use rust_rest::note::NoteId;
+use rust_rest::posts::Post;
+use rust_rest::posts::PostUpdate;
+use rust_rest::posts::NewPost;
+use rust_rest::posts::PostId;
+
+use rust_rest::posts::Reply;
+use rust_rest::posts::ReplyUpdate;
+use rust_rest::posts::NewReply;
+use rust_rest::posts::ReplyId;
 
 #[macro_use]
 extern crate rocket;
 
 #[derive(Database)]
-#[database("notes")]
-struct NotesDatabase(sqlx::PgPool);
+#[database("posts")]
+struct PostsDatabase(sqlx::PgPool);
 
 struct DatabaseError(rocket_db_pools::sqlx::Error);
 
@@ -32,67 +37,150 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-/* fn open_note(path: &str) -> File {
-    OpenOptions::new()
-        .read(true)
-        .append(true)
-        .create(true)
-        .open(path)
-        .expect("unable to access file")
-}
-
-fn open_temp_note(path: &str) -> File {
-    OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(path)
-        .expect("unable to access new_notes.txt")
-} */
-
-#[post("/createnote", data = "<note>")]
-async fn create_note(note: Json<NewNote<'_>>, mut db: Connection<NotesDatabase>) -> Result<Json<Note>, DatabaseError> {
-    let new_note = sqlx::query_as::<_, Note>("INSERT INTO notes (content) VALUES ($1) RETURNING *")
-        .bind(note.content)
+// POST CRUD
+#[post("/newpost", data = "<new_post>")]
+async fn create_post(new_post: Json<NewPost<'_>>, mut db: Connection<PostsDatabase>) -> Result<Json<Post>, DatabaseError> {
+    let new_posts = sqlx::query_as::<_, Post>("INSERT INTO posts (content) VALUES ($1) RETURNING *")
+        .bind(new_post.content)
         .fetch_one(&mut *db)
         .await?;
 
-    Ok(Json(new_note))
+    Ok(Json(new_posts))
 }
 
-#[get("/readnotes")]
-async fn read_notes(mut db: Connection<NotesDatabase>) -> Result<Json<Vec<Note>>, DatabaseError> {
-    let notes = sqlx::query_as::<_, Note>("SELECT * FROM notes")
+#[get("/getposts")]
+async fn get_all_posts(mut db: Connection<PostsDatabase>) -> Result<Json<Vec<Post>>, DatabaseError> {
+    let posts = sqlx::query_as::<_, Post>("SELECT * FROM posts")
         .fetch_all(&mut *db)
         .await?;
 
-    Ok(Json(notes))
+    Ok(Json(posts))
 }
 
-#[put("/updatenote", data = "<note_update>")]
-async fn update_note(note_update: Json<Note>, mut db: Connection<NotesDatabase>) -> Result<Json<Note>, DatabaseError> {
-    let updated_note = sqlx::query_as::<_, Note>("UPDATE notes SET content = $1 WHERE id = $2 RETURNING *")
-        .bind(&note_update.content)
-        .bind(note_update.id)
+#[put("/editpost", data = "<update_post>")]
+async fn edit_post(update_post: Json<PostUpdate>, mut db: Connection<PostsDatabase>) -> Result<Json<Post>, DatabaseError> {
+    let updated_posts = sqlx::query_as::<_, Post>("UPDATE posts SET content = $1 WHERE id = $2 RETURNING *")
+        .bind(&update_post.content)
+        .bind(update_post.id)
         .fetch_one(&mut *db)
         .await?;
 
-    Ok(Json(updated_note))
+    Ok(Json(updated_posts))
 }
 
-#[delete("/deletenote", data = "<note_id>")]
-async fn delete_note(note_id: Json<NoteId>, mut db: Connection<NotesDatabase>) -> Result<Json<Note>, DatabaseError> {
-    let deleted_note = sqlx::query_as::<_, Note>("DELETE FROM notes WHERE id = $1 RETURNING *")
-        .bind(note_id.id)
+#[delete("/deletepost", data = "<post_id>")]
+async fn delete_post(post_id: Json<PostId>, mut db: Connection<PostsDatabase>) -> Result<Json<Post>, DatabaseError> {
+    let deleted_posts = sqlx::query_as::<_, Post>("DELETE FROM posts WHERE id = $1 RETURNING *")
+        .bind(post_id.id)
         .fetch_one(&mut *db)
         .await?;
 
-    Ok(Json(deleted_note))
+    Ok(Json(deleted_posts))
+}
+
+// REPLY CRUD
+#[post("/<post_id>/newreply", data = "<new_reply>")]
+async fn create_reply(new_reply: Json<NewReply<'_>>, post_id: i64 , mut db: Connection<PostsDatabase>) -> Result<Json<Reply>, DatabaseError> {
+    let new_reply = sqlx::query_as::<_, Reply>("INSERT INTO replies (postid, content) VALUES ($1, $2) RETURNING *") //  WHERE postid = $1
+        .bind(post_id)
+        .bind(new_reply.content)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(new_reply))
+}
+
+#[get("/<post_id>/getreplies")]
+async fn get_replies(post_id: i64 , mut db: Connection<PostsDatabase>) -> Result<Json<Vec<Reply>>, DatabaseError> {
+    let replies = sqlx::query_as::<_, Reply>("SELECT * FROM replies WHERE postid = $1")
+        .bind(post_id)
+        .fetch_all(&mut *db)
+        .await?;
+
+    Ok(Json(replies))
+}
+
+#[put("/<post_id>/editreply", data = "<reply>")]
+async fn edit_reply(reply: Json<ReplyUpdate>, post_id: i64 , mut db: Connection<PostsDatabase>) -> Result<Json<Reply>, DatabaseError> {
+    let updated_replies = sqlx::query_as::<_, Reply>("UPDATE replies SET content = $1 WHERE postid = $2 AND id = $3 RETURNING *") //  WHERE postid = $2 AND id = $3
+        .bind(&reply.content)
+        .bind(post_id)
+        .bind(reply.id)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(updated_replies))
+}
+
+#[delete("/<post_id>/deletereply", data = "<reply_id>")]
+async fn delete_reply(reply_id: Json<ReplyId>, post_id: i64 , mut db: Connection<PostsDatabase>) -> Result<Json<Reply>, DatabaseError> {
+    let deleted_replies = sqlx::query_as::<_, Reply>("DELETE FROM replies WHERE postid = $1 AND id = $2 RETURNING *")
+        .bind(post_id)
+        .bind(reply_id.id)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(deleted_replies))
+}
+
+// LIKES
+#[patch("/<post_id>/likepost")]
+async fn like_post(post_id: i64, mut db: Connection<PostsDatabase>) -> Result<Json<Post>, DatabaseError> {
+    let liked_post = sqlx::query_as::<_, Post>("UPDATE posts SET likes = likes + 1 WHERE id = $1 RETURNING *")
+        .bind(post_id)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(liked_post))
+}
+
+#[patch("/<post_id>/dislikepost")]
+async fn dislike_post(post_id: i64, mut db: Connection<PostsDatabase>) -> Result<Json<Post>, DatabaseError> {
+    let liked_post = sqlx::query_as::<_, Post>("UPDATE posts SET likes = likes - 1 WHERE id = $1 RETURNING *")
+        .bind(post_id)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(liked_post))
+}
+
+#[patch("/<post_id>/likereply", data = "<reply_id>")]
+async fn like_reply(reply_id: Json<ReplyId>, post_id: i64, mut db: Connection<PostsDatabase>) -> Result<Json<Reply>, DatabaseError> {
+    let liked_reply = sqlx::query_as::<_, Reply>("UPDATE replies SET likes = likes + 1 WHERE postid = $1 AND id = $2 RETURNING *")
+        .bind(post_id)
+        .bind(reply_id.id)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(liked_reply))
+}
+
+#[patch("/<post_id>/dislikereply", data = "<reply_id>")]
+async fn dislike_reply(reply_id: Json<ReplyId>, post_id: i64, mut db: Connection<PostsDatabase>) -> Result<Json<Reply>, DatabaseError> {
+    let liked_reply = sqlx::query_as::<_, Reply>("UPDATE replies SET likes = likes - 1 WHERE postid = $1 AND id = $2 RETURNING *")
+        .bind(post_id)
+        .bind(reply_id.id)
+        .fetch_one(&mut *db)
+        .await?;
+
+    Ok(Json(liked_reply))
+}
+
+#[get("/latest")]
+async fn get_latest(mut db: Connection<PostsDatabase>) -> Result<Json<Vec<Post>>, DatabaseError> {
+    let latest_posts = sqlx::query_as::<_, Post>("SELECT * FROM posts ORDER BY post_date DESC LIMIT 50")
+    .fetch_all(&mut *db)
+    .await?;
+
+    Ok(Json(latest_posts))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .attach(NotesDatabase::init())
-    .mount("/", routes![index, create_note, read_notes, update_note, delete_note])
+    .attach(PostsDatabase::init())
+    .mount("/", routes![index, create_post, get_all_posts, edit_post, delete_post])
+    .mount("/", routes![create_reply, get_replies, edit_reply, delete_reply])
+    .mount("/", routes![like_post, dislike_post, like_reply, dislike_reply])
+    .mount("/", routes![get_latest])
 }
